a8722fd7fd8164bbc19c704c14dd74a332a9b48a
Falke Van Hemeldonck <falkevanhemeldonck@gmail.com>
2023-04-05 00:05:42 +0200

    gegevensvalidatie en aanpassing invulveld gsm nr

--- registreer_pagina/registreer.js
+++ registreer_pagina/registreer.js
@@ -1,31 +1,31 @@
-  const form = document.querySelector('form');
-  const postcodeInput = document.querySelector('#postcode');
-  const postcodeError = document.querySelector('#postcode-error');
+const form = document.querySelector('form');
+const postcodeInput = document.querySelector('#postcode');
+const postcodeError = document.querySelector('#postcode-error');
 
 
-  form.addEventListener('submit', (event) => {
-    if (postcodeInput.validity.patternMismatch) {
-      event.preventDefault();
-      postcodeError.textContent = 'Postcode moet uit exact 4 cijfers bestaan.';
-    } else {
-      postcodeError.textContent = '';
-    }
-  });
-
-
-  function validateForm() {
-    var password = document.getElementById("password");
-    var confirm_password = document.getElementById("confirm_password");
-    var error_message = document.getElementById("password_error");
-  
-    if (password.value != confirm_password.value) {
-      error_message.innerHTML = "Wachtwoorden komen niet overeen";
-      return false;
-    } else {
-      error_message.innerHTML = "";
-      return true;
-    }
+form.addEventListener('submit', (event) => {
+  if (postcodeInput.validity.patternMismatch) {
+    event.preventDefault();
+    postcodeError.textContent = 'Postcode moet uit exact 4 cijfers bestaan.';
+  } else {
+    postcodeError.textContent = '';
   }
+});
+
+
+function validateForm() {
+  var password = document.getElementById("password");
+  var confirm_password = document.getElementById("confirm_password");
+  var error_message = document.getElementById("password_error");
+
+  if (password.value != confirm_password.value) {
+    error_message.innerHTML = "Wachtwoorden komen niet overeen";
+    return false;
+  } else {
+    error_message.innerHTML = "";
+    return true;
+  }
+}
 
 const usernameInput = document.getElementById("username");
 const usernameValidation = document.getElementById("username-validation");
@@ -52,12 +52,11 @@ function checkUsername() {
 }
 
 
-const form = document.querySelector('form');
 const successPopup = document.querySelector('#success-popup');
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
-  if (validateForm()) {
+  if (true) {
     successPopup.style.display = 'block';
     form.reset();
     setTimeout(() => {
@@ -66,5 +65,17 @@ form.addEventListener('submit', (event) => {
   }
 });
 
+const gsmInput = document.getElementById("gsm");
+const gsmError = document.getElementById("gsm-error");
+
+form.addEventListener('submit', (event) => {
+  const gsmPattern = /(\+32\s\d{2}\s\d{3}\s\d{2}\s\d{2})?/;
+  if (!gsmPattern.test(gsmInput.value)) {
+    event.preventDefault();
+    gsmError.textContent = 'Voer een geldig Belgisch GSM-nummer in (bijvoorbeeld: +32 49 123 45 67).';
+  } else {
+    gsmError.textContent = '';
+  }
+});
 
 
